77569a711169e9976a2147ba58fa82e87703a3cb
hpu339 <15513924345@163.com>
2023-10-09 22:02:43 +0800

    添加工程目录

---
+++ Project_template/USER/Objects/ch32f20x_it.d
@@ -0,0 +1,9 @@
+./objects/ch32f20x_it.o: ch32f20x_it.c ..\USER\ch32f20x_it.h \
+  ..\Platform\INC\ch32f20x.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h ..\CORE\core_cm3.h \
+  C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
+  ..\USER\system_ch32f20x.h ..\USER\ch32f20x_conf.h \
+  ..\Platform\INC\ch32f20x_gpio.h ..\Platform\INC\ch32f20x_rcc.h \
+  ..\Platform\INC\ch32f20x_misc.h
---
+++ Project_template/USER/main.c
@@ -0,0 +1,23 @@
+/*
+ * @Author: Lixin Yang
+ * @Date: 2023-10-08 23:26:26
+ * @LastEditTime: 2023-10-08 23:31:24
+ * @FilePath: \.cmsise:\CH32F208WBU6\CH32F208\Project_template\USER\main.c
+ * @Description: 
+ * 
+ */
+
+
+#include "ch32f20x.h"
+#include "ch32f20x_conf.h"
+#include "stdio.h"	
+
+
+int main( void )
+{
+
+    while( 1 )
+    {
+
+    }
+}
